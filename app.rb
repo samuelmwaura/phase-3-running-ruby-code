d6fa3abacb96a5ab975ec0ec348afde753f3175b
Samuel Mwaura <samuelmwaura081@gmail.com>
2022-08-12 17:58:23 +0300

    Completed the assignment

---
+++ app.rb
@@ -0,0 +1,18 @@
+# #The first ruby comment.
+# #puts adds a newline to the end of the line unlike prints
+puts 'Hello World!'
+# puts 'This is ruby'
+
+print 'Pass this test, please.'  #no linebrak added at the end.
+# puts 'This is ruby'
+
+# puts [1,2,3] #converts  everything to a string using the to_s method and prints each element ina  new line
+p [1,2,3] #calls .inspect on the array and output the array as it is.
+
+
+# #pretty - printing 
+
+# pp [{id:1,hello:"world"},{id:2, hello:"ruby"},{id:3, hello: "Moon"},{id:4, hello:"Learner"}]
+# #IRB- interactive ruby code- a REPL like the Js Browser console.
+
+# #Ruby uses snake case i.e using underscore between variable names 
